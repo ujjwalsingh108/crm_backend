216eaf2eba10c7c8a5c79d0147b095c038689365
ujjwal-ampcome <ujjwal.singh@ampcome.com>
2024-12-30 17:32:29 +0530

    base setup extended

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.SERVER_TYPE as 'mssql',
+      host: process.env.SERVER_NAME,
+      port: 1433,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      options: {
+        encrypt: true,
+      },
+      extra: {
+        authentication: {
+          type: 'NTLM',
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
